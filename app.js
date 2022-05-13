c878b74b70493a4a8028417cc264612c4b5c7de3
Sangjin Lee <isangjin77@gmail.com>
2022-05-13 14:31:44 +0900

    add body-parser, jwttoken module

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // load the dependencies
 const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 // load config.js
 const config = require('./config')
@@ -9,11 +11,22 @@ const PORT = process.env.PORT || config.serverPort
 // express configuration
 const app = express()
 
+// parse JSON and url-encoded query
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json())
+
 // index page just for testing.
 app.get('/', (request, response) => {
     response.send('Hello JWT')
 })
 
+// configure express router API
+app.use('/api', require('./routes/api'))
+
+//set the secret key variable for jwt
+app.set('jwt-secret', config.secretKey)
+
+// open backend server
 app.listen(PORT, () => {    // ./config.js
     console.log('Express server running on port', PORT)
 })
@@ -25,7 +38,7 @@ mongoose.connect(config.mongodbUri, {
     // useUnifiedTopology : true,
     // useCreateIndex : true,
     // useFindAndModify : false
-}).then( () => console.log("mongoDB connection establish...") )
+}).then( () => console.log("mongoDB connection establish... Done") )
 .catch(err => console.log(err))
 
 const db = mongoose.connection
@@ -34,3 +47,4 @@ db.once('open', () => {
     console.log("successfully connected to mongoDB")
 })
 
+app.use(express.static(path.join((__dirname, 'scripts'))))
