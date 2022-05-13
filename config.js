abb171c3e31f7a715f4f82756b8339bb3811d85b
Sangjin Lee <isangjin77@gmail.com>
2022-05-13 14:32:12 +0900

    modify configuration file and package.json

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'secret': '#',          // replace this with your secret key
+    'secretKey': 'SeCrEtKeYErEaSeThIs',          // replace this with your secret key
     'mongodbUri': 'mongodb://127.0.0.1:27017/devOneLink',
     'serverPort' : 8080,    // can be replaced by your own port
     'serverName' : "http://127.0.0.1:8080/",
